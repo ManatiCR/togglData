8038a4627090629c3669068ea78e557226ec574d
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2015-10-07 17:02:08 -0600

    added createClient function

--- togglData.py
+++ togglData.py
@@ -4,14 +4,14 @@ import base64
 import mysql.connector as mysql
 import json
 
-string=''+':api_token'
-headers={'Authorization':'Basic '+base64.b64encode(string)}
-url='https://www.toggl.com/api/v8/me'
-response=requests.get(url,headers=headers)
+string = ''+':api_token'
+headers = {'Authorization':'Basic '+base64.b64encode(string)}
+url = 'https://www.toggl.com/api/v8/me'
+response = requests.get(url,headers=headers)
 if response.status_code!=200:
     print "Login failed. Check your API key"
     quit()
-response=response.json()
+response = response.json()
 
 mysqldb_config = {
    'user': 'root',
@@ -34,12 +34,11 @@ def workspaceExist(workspacesId, mysqldb_connection):
     cursor.execute(query, queryData)
     for row in cursor:
         if row != '':
+            cursor.close()
             return True
         else:
+            cursor.close()
             return False
-
-    cursor.close()
-
 
 def createWorkspace(workspace, mysqldb_connection):
     """This function create a single workspace"""
@@ -58,7 +57,40 @@ def createWorkspace(workspace, mysqldb_connection):
         cursor.close()
 
 
+def createProject(project, mysqldb_connection):
+    """This function create a single project"""
+    cursor = mysqldb_connection.cursor()
+    
+
+
+def createClient(client, mysqldb_connection):
+    """This function create a single client"""
+    cursor = mysqldb_connection.cursor()
+    addClient = ("INSERT INTO client "
+                "(cid, wid, name)"
+                "VALUES (%(cid)s, %(wid)s, %(name)s)")
+    clientData = {
+    'cid': client['id'],
+    'wid': client['wid'],
+    'name': client['name'],
+    }
+    cursor.execute(addClient, clientData)
+    mysqldb_connection.commit()
+    cursor.close()
+
+
 for workspace in workspaces:
-    #projects = requests.get('https://www.toggl.com/api/v8/workspaces/'+str(workspace['id'])+'/projects', headers=headers)
-    #projects = projects.json()
+    clients = requests.get('https://www.toggl.com/api/v8/workspaces/'+str(workspace['id'])+'/clients', headers=headers)
+    clients = clients.json()
+    projects = requests.get('https://www.toggl.com/api/v8/workspaces/'+str(workspace['id'])+'/projects', headers=headers)
+    projects = projects.json()
+    # Create a  workspace
     createWorkspace(workspace, mysqldb_connection)
+
+    #create all the clients of one workspace
+    for client in clients:
+        createClient(client, mysqldb_connection)
+
+    #Create all the projects of one workspace
+    for project in projects:
+        createProject(project, mysqldb_connection)
