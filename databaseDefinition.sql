60e86e27c45eb355fcb0266a60f3234c9880ed40
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2015-10-07 17:01:17 -0600

    Added basic database definition

---
+++ databaseDefinition.sql
@@ -0,0 +1,22 @@
+CREATE TABLE workspace (
+wid int NOT NULL, 
+name varchar(120) NOT NULL,
+PRIMARY KEY (wid));
+
+CREATE TABLE client (
+cid int NOT NULL,
+wid int NOT NULL,
+name varchar(120) NOT NULL,
+PRIMARY KEY (cid),
+FOREIGN KEY (wid) REFERENCES workspace(wid)
+);
+
+CREATE TABLE project (
+pid int NOT NULL,
+wid int NOT NULL,
+cid int NOT NULL,
+name varchar(120) NOT NULL,
+PRIMARY KEY (pid),
+FOREIGN KEY (wid) REFERENCES workspace(wid),
+FOREIGN KEY (cid) REFERENCES client(cid)
+);
