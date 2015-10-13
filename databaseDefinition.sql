3b0dbb995b2473567dc3d0367b5b20284cc5d676
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2015-10-13 17:11:53 -0600

    added time_entry table

--- databaseDefinition.sql
+++ databaseDefinition.sql
@@ -20,3 +20,25 @@ PRIMARY KEY (pid),
 FOREIGN KEY (wid) REFERENCES workspace(wid),
 FOREIGN KEY (cid) REFERENCES client(cid)
 );
+
+CREATE  TABLE user (
+  uid  int NOT NULL,
+  default_wid int NOT NULL,
+  fullname varchar(255) NOT NULL,
+  PRIMARY KEY (uid),
+  FOREIGN KEY  (default_wid) REFERENCES workspace(wid)
+);
+
+CREATE TABLE time_entry (
+  teid int NOT NULL,
+  pid int NOT NULL,
+  uid int NOT NULL,
+  description LONGTEXT,
+  start_date  DATETIME NOT NULL,
+  stop_date DATETIME NOT NULL,
+  duration DOUBLE NOT NULL 
+  updated DATETIME NOT NULL,
+
+
+
+)
