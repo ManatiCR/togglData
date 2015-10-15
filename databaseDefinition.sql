a735230bfeca29484ea8d662c33bb294dd1da6c8
Rodrigo Espinoza <espinoza.brenes.rodrigo@gmail.com>
2015-10-14 18:51:08 -0600

    added unsigned property to int fields

--- databaseDefinition.sql
+++ databaseDefinition.sql
@@ -1,20 +1,20 @@
 CREATE TABLE workspace (
-wid int NOT NULL,
+wid int unsigned NOT NULL,
 name varchar(120) NOT NULL,
 PRIMARY KEY (wid));
 
 CREATE TABLE client (
-cid int NOT NULL,
-wid int NOT NULL,
+cid int unsigned NOT NULL,
+wid int unsigned NOT NULL,
 name varchar(120) NOT NULL,
 PRIMARY KEY (cid),
 FOREIGN KEY (wid) REFERENCES workspace(wid)
 );
 
 CREATE TABLE project (
-pid int NOT NULL,
-wid int NOT NULL,
-cid int NOT NULL,
+pid int unsigned NOT NULL,
+wid int unsigned NOT NULL,
+cid int unsigned NOT NULL,
 name varchar(120) NOT NULL,
 PRIMARY KEY (pid),
 FOREIGN KEY (wid) REFERENCES workspace(wid),
@@ -22,23 +22,23 @@ FOREIGN KEY (cid) REFERENCES client(cid)
 );
 
 CREATE  TABLE user (
-  uid  int NOT NULL,
-  default_wid int NOT NULL,
+  uid int unsigned NOT NULL,
+  default_wid int unsigned NOT NULL,
   fullname varchar(255) NOT NULL,
   PRIMARY KEY (uid),
   FOREIGN KEY  (default_wid) REFERENCES workspace(wid)
 );
 
 CREATE TABLE time_entry (
-  teid int NOT NULL,
-  pid int NOT NULL,
-  uid int NOT NULL,
+  teid int unsigned NOT NULL,
+  pid int unsigned NOT NULL,
+  uid int unsigned NOT NULL,
   description LONGTEXT,
-  start_date  DATETIME NOT NULL,
+  start_date DATETIME NOT NULL,
   stop_date DATETIME NOT NULL,
-  duration DOUBLE NOT NULL 
+  duration DOUBLE NOT NULL,
   updated DATETIME NOT NULL,
-
-
-
-)
+  PRIMARY KEY (teid),
+  FOREIGN KEY (uid) REFERENCES user(uid),
+  FOREIGN KEY (pid) REFERENCES project(pid)
+);
